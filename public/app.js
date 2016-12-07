acb6601f04d8ad0b8280d00c535b937d112d444d
Josephine Elder <parusmontanus@gmail.com>
2016-12-07 16:24:38 +0000

    Wrote makeRequest and requestComplete functions

--- public/app.js
+++ public/app.js
@@ -1,5 +1,41 @@
-var app = function(){
 
+var app = function() {
+    var url = 'https://api.spotify.com/v1/search?q=christmas&type=album';
+    makeRequest(url, requestComplete);
 }
 
+var makeRequest = function(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = callback;
+    request.send();
+}
+
+var requestComplete = function() {
+    console.log('request successful');
+    if (this.status !== 200) return;
+
+    var jsonString = this.responseText;
+    var albums = JSON.parse(jsonString);
+    populateList(albums);
+}
+
+
+
+
+
+
+
+
+
+
+
 window.onload = app;
+
+
+
+
+
+
+
+
